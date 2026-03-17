dbd0f04abc2f70245ccb1124ae5ca308b8c58882
pnduta <kabute.perpetua@eclectics.io>
2026-03-17 11:24:35 +0300

    Initial Commit

---
+++ CoopDuka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoopDuka
+//
+//  Created by Perpetua Kabute    on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoopDuka/CoopDukaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoopDukaApp.swift
+//  CoopDuka
+//
+//  Created by Perpetua Kabute    on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoopDukaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
